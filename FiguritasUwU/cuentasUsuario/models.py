bb1966f7125391c389f93842764996aff53defce
jozk <jorgeriffo.u@gmail.com>
2023-06-28 13:17:50 -0400

    pag v2

--- FiguritasUwU/cuentasUsuario/models.py
+++ FiguritasUwU/cuentasUsuario/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
+    nombreUsuario=models.CharField(max_length=50, null=True)
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField(blank=True, null=True)
-    contraseña = models.CharField(max_length=50)
+    contrasenia = models.CharField(max_length=50)
     direccion = models.CharField(max_length=200, verbose_name="Dirección cliente")
     telefono = models.CharField(max_length=9)
     ciudad = models.CharField(max_length=50)
--- FiguritasUwU/pedidos/views.py
+++ FiguritasUwU/pedidos/views.py
@@ -5,8 +5,10 @@ from django.db.models import Q
 # Create your views here.
 
 
-def carrito(request):
+def nuevoPedido(request):
 
-    productos = list(detallePedido.objects.select_related("producto").filter(pedido__id=))
+    detallePedido=detallePedido();
+
+    productos = list(detallePedido.objects.select_related("pedido").filter(pedido__id=detallePedido.id))
 
     return render(request, 'pedidos/carrito_compra.html', {"productos":productos, })
